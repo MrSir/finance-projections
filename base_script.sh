4dc1420c327fcff02db09f9bd646e3dc5c325cfa
Mitko Tochev <mtochev@engagepeople.com>
2017-01-24 21:44:28 -0500

    more basic setup

--- base_script.sh
+++ base_script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-ln -s /opt/VBoxGuestAdditions-4.3.10/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
 
 # shits and giggles #
 alias fucking=sudo
@@ -8,80 +7,78 @@ apt-get update
 apt-get -y install python-software-properties
 
 # add needed repos and update #
-apt-add-repository --yes ppa:ondrej/apache2
-apt-add-repository --yes ppa:ondrej/php5-5.6
-apt-add-repository --yes ppa:chris-lea/node.js
 apt-get update
 
 # basic tools #
-apt-get -y install vim curl
+apt-get -y install vim curl nginx sqlite
 
-# APACHE STUFF should be v2.2.22#
-apt-get -y install apache2
+# install php and enable it should be v7.0#
+apt-get -y install php7.0 php-mcrypt php-curl php-gd php-imap php-xdebug php-xml php-mbstring php-zip php-soap php7.0-sqlite php7.0-bcmath
+phpenmod mcrypt
+phpenmod gd
+phpenmod imap
 
-# ADD the virtual host #
-cp /var/www/finances/site.conf /etc/apache2/sites-available/finances.conf
-ln -s /etc/apache2/sites-available/finances.conf /etc/apache2/sites-enabled/finances.conf
+# configure php upload file size to something more usable
+sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 128M/" /etc/php/7.0/fpm/php.ini
+sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 128M/" /etc/php/7.0/cli/php.ini
+sed -i "s/post_max_size = 8M/post_max_size = 128M/" /etc/php/7.0/fpm/php.ini
+sed -i "s/post_max_size = 8M/post_max_size = 128M/" /etc/php/7.0/cli/php.ini
 
-# give apache some settings #
-echo "ServerName engagepeople.com" >> /etc/apache2/apache2.conf
-echo "DocumentRoot /var/www" >> /etc/apache2/apache2.conf
+service php7.0-fpm restart
+service nginx restart
 
-# enable mod_rewrite #
-a2enmod rewrite
-
-# install php5 and enable it should be v5.4.3#
-apt-get -y install php5 php5-mcrypt php5-curl php5-gd php5-imap
-php5enmod mcrypt
-php5enmod gd
-php5enmod imap
-
-apt-get -y install libapache2-mod-php5
-a2enmod php5
-
-# restart apache2 #
-service apache2 restart
+# configure xdebug #
+echo "xdebug.remote_enable = 1" >> /etc/php/7.0/mods-available/xdebug.ini
+echo "xdebug.remote_connect_back = 1" >> /etc/php/7.0/mods-available/xdebug.ini
+echo "xdebug.remote_port = 9000" >> /etc/php/7.0/mods-available/xdebug.ini
+echo "xdebug.scream=0" >> /etc/php/7.0/mods-available/xdebug.ini
+echo "xdebug.cli_color=1" >> /etc/php/7.0/mods-available/xdebug.ini
+echo "xdebug.show_local_vars=1" >> /etc/php/7.0/mods-available/xdebug.ini
 
 # install mysql-server #
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password war0662'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password war0662'
-apt-get -y install mysql-server php5-mysql
-apt-get -y install libapache2-mod-auth-mysql
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password Test1234'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password Test1234'
+apt-get -y install mysql-server php-mysql
 
 # add root user to specific IP to differentiate between vagrant boxes #
-mysql -uroot -pwar0662 -e "CREATE USER 'root'@'%' IDENTIFIED BY 'war0662'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
-# add rawdev users #
-mysql -uroot -pwar0662 -e "CREATE USER 'dev'@'localhost' IDENTIFIED BY 'Test1234'; GRANT ALL PRIVILEGES ON *.* TO 'dev'@'localhost';"
+mysql -uroot -pTest1234 -e "CREATE USER 'root'@'%' IDENTIFIED BY 'Test1234'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
 
-# create tdrewards related databases #
-mysql -uroot -pwar0662 -e "CREATE DATABASE finances CHARSET utf8 COLLATE utf8_unicode_ci;"
+# create promo2016 db #
+mysql -uroot -pTest1234 -e "CREATE DATABASE finance-projections CHARSET utf8 COLLATE utf8_unicode_ci;"
 
-# add permissions to rawdev, rawdev2, rawdev3 for the tdrewards tables #
-mysql -uroot -pwar0662 -e "GRANT ALL PRIVILEGES ON finances.* TO 'dev'@'localhost';"
+# add permissions to root for promo2016 tables #
+mysql -uroot -pTest1234 -e "GRANT ALL PRIVILEGES ON finance-projections.* TO 'root'@'localhost';"
 
-# restart apache2 #
-service apache2 restart
-
-# change mysql config to allow connections to it #
-sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+# change mysql config  #
+sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i "s/max_allowed_packet.*/max_allowed_packet = 128M/" /etc/mysql/mysql.conf.d/mysqld.cnf
 
 # restart mysql #
 service mysql restart
+
+# install nodejs v0.10.29 #
+apt-get -y install nodejs
+apt-get -y install npm
+
+npm set progress=false
+
+sudo ln -s /usr/bin/nodejs /usr/bin/node
 
 # install composer globally #
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
-# install nodejs v0.10.29 #
-apt-get -y install nodejs
-
-# install npm v1.4.14 #
-apt-get -y install npm
-
 # install bower v1.3.8 globally #
 npm install bower -g
+
+npm install apidoc -g
 
 # install git #
 apt-get -y install git
 
 apt-get autoclean
+
+# site config copy
+cp /var/www/finance-projections/site.conf /etc/nginx/sites-enabled/finance-projections.conf
+
+service nginx restart
