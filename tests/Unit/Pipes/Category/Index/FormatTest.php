8c488261b3beefcfb4afff04f33f990b42a51507
Mitko Tochev <mtochev@engagepeople.com>
2017-05-25 17:25:23 -0400

    added format tests for index

---
+++ tests/Unit/Pipes/Category/Index/FormatTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Tests\Unit\Pipes\Category\Index;
+
+use App\Models\Category;
+use App\Passables\Category\Index;
+use App\Pipes\Category\Index\Format as PipeFormat;
+use App\Tests\Unit\Pipes\Index\Format;
+use Exception;
+
+/**
+ * Class FormatTest
+ * @package App\Tests\Unit\Pipes\Category\Index
+ */
+class FormatTest extends Format
+{
+    /**
+     * SearchTest constructor.
+     *
+     * @param null   $name
+     * @param array  $data
+     * @param string $dataName
+     */
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct(
+            $name,
+            $data,
+            $dataName
+        );
+
+        $this->setPassable(Index::class);
+        $this->setPipe(PipeFormat::class);
+        $this->setModel(Category::class);
+    }
+
+    /**
+     * @group App
+     * @group App.Pipes
+     * @group App.Pipes.Category
+     * @group App.Pipes.Category.Index
+     * @group App.Pipes.Category.Index.Format
+     * @group App.Pipes.Category.Index.Format.Success
+     */
+    public function testFormatSuccess()
+    {
+        $this->formatSuccess();
+    }
+
+    /**
+     * @group                    App
+     * @group                    App.Pipes
+     * @group                    App.Pipes.Category
+     * @group                    App.Pipes.Category.Index
+     * @group                    App.Pipes.Category.Index.Format
+     * @group                    App.Pipes.Category.Index.Format.Failure
+     * @expectedExceptionCode    500
+     * @expectedException Exception
+     * @expectedExceptionMessage Category format failed.
+     */
+    public function testFormatFailure()
+    {
+        $this->formatFailure();
+    }
+}
---
+++ tests/Unit/Pipes/Index/Format.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Tests\Unit\Pipes\Index;
+
+use App\Passables\Account\Index;
+use App\Tests\TestCase;
+use Exception;
+
+/**
+ * Class Format
+ * @package App\Tests\Unit\Pipes\Index
+ */
+class Format extends TestCase
+{
+    /**
+     * @var string
+     */
+    protected $pipe;
+
+    /**
+     * @var string
+     */
+    protected $passable;
+
+    /**
+     * @var string
+     */
+    protected $model;
+
+    /**
+     * @return string
+     */
+    public function getPipe()
+    {
+        return $this->pipe;
+    }
+
+    /**
+     * @param string $pipe
+     */
+    public function setPipe($pipe)
+    {
+        $this->pipe = $pipe;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassable()
+    {
+        return $this->passable;
+    }
+
+    /**
+     * @param string $passable
+     */
+    public function setPassable($passable)
+    {
+        $this->passable = $passable;
+    }
+
+    /**
+     * @return string
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * @param string $model
+     */
+    public function setModel($model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * Format Success
+     */
+    public function formatSuccess()
+    {
+        $passableClass = $this->getPassable();
+        $pipeClass = $this->getPipe();
+        $modelClass = $this->getModel();
+
+        $results = collect(
+            [
+                $modelClass::find(2),
+            ]
+        );
+
+        $passable = new $passableClass();
+        $passable->setResults($results);
+        $passable->setPerPage(10);
+        $passable->setPage(3);
+        $passable->setTotals(1);
+
+        $pipe = new $pipeClass();
+
+        $pipe->handle(
+            $passable,
+            function ($passable) {
+                $results = $passable->getResponse();
+
+                $this->assertEquals(
+                    200,
+                    $results['code']
+                );
+                $this->assertEquals(
+                    1,
+                    $results['totals']
+                );
+                $this->assertEquals(
+                    10,
+                    $results['per_page']
+                );
+                $this->assertEquals(
+                    3,
+                    $results['page']
+                );
+                $this->assertEquals(
+                    2,
+                    $results['results'][0]->id
+                );
+            }
+        );
+    }
+
+    /**
+     * Format Failure
+     */
+    public function formatFailure()
+    {
+        $passableClass = $this->getPassable();
+        $pipeClass = $this->getPipe();
+
+        $passable = new $passableClass();
+
+        // force the page getter to throw an exception
+        $passable->setPage(
+            function () {
+                throw new Exception('test');
+            }
+        );
+
+        $pipe = new $pipeClass();
+        $pipe->handle(
+            $passable,
+            function ($passable) {
+                //do nothing
+            }
+        );
+    }
+}
